cd594d1ea3a101f312147fa39b54b250a38097fd
Jef <jackefransham@gmail.com>
2018-10-29 13:52:48 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//! This is a stub to reserve the name on crates.io and docs.rs until Substrate is properly released.
+//!
+//! For up-to-date information, see the [project page on GitHub](https://github.com/paritytech/substrate).
+
+fn main() {
+    println!("Hello, world!");
+}
